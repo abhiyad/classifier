6b5de55a36e55a2e85196ae01b487068dc07d176
Abhishek Yadav <abhishekcms611@gmail.com>
2018-01-13 15:29:50 +0530

    :+1:

---
+++ src/client_py2.py
@@ -0,0 +1,28 @@
+ #!/usr/bin/env python
+import sys
+import rospy
+from cpppy.srv import *
+
+def add_two_ints_client(x, y):
+    rospy.wait_for_service('add_two_ints')
+    try:
+        add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
+        resp1 = add_two_ints(x, y)
+        return resp1.sum
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+def usage():
+    return "%s [x y]"%sys.argv[0]
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        x = int(sys.argv[1])
+        y = int(sys.argv[2])
+    else:
+        print usage()
+        sys.exit(1)
+    for i in range (100):
+        print "Requesting %s+%s"%(i, 20+i)
+
+        print "%s + %s = %s"%(i, 20+i, add_two_ints_client(i, 20+i))
